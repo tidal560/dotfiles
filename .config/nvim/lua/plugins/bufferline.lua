67bdb61bb321fd6da368b28e971927017ff1651a
tidal560 <tidalwave560@anglernook.com>
2025-11-30 21:59:08 +0000

    add tab behavior to tab line at the top of nvim file

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -4,8 +4,8 @@ return{
     dependencies = 'nvim-tree/nvim-web-devicons',
     opts = {
         options = {
+            mode = "tabs",
             separator_style = "slant",
         },
     },
-
 }

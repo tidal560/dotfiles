8e9774bc06f34c907edc7d54758013ca18fc7305
tidal560 <tidalwave560@anglernook.com>
2025-11-30 22:08:29 +0000

    modify lsp configuration

--- .config/nvim/lua/plugins/mason_lspconfig.lua
+++ .config/nvim/lua/plugins/mason_lspconfig.lua
@@ -1,24 +1,25 @@
 return{
-    {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {
-            ensure_installed = {
-                "lua_ls",
-                "clangd",
-                "pylsp",
-            },
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+        ensure_installed = {
+            "lua_ls",
+            "clangd",
+            "pylsp",
         },
     },
-    {
-        "mason-org/mason.nvim",
-        opts = {
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
+    dependencies = {
+        {
+            "mason-org/mason.nvim",
+            opts = {
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "➜",
+                        package_uninstalled = "✗"
+                    }
+                },
             },
-        }
+        },
+        "neovim/nvim-lspconfig",
     },
 }

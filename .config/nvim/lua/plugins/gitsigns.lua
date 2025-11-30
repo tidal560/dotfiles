b4cef63adcc0056af7cd1a5e878978e3d9a5efb8
tidal560 <tidalwave560@anglernook.com>
2025-11-30 21:57:24 +0000

    add basic githighlights, keymap for gitdiff

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,17 @@
+return {
+    "lewis6991/gitsigns.nvim",
+
+    opts = {
+        on_attach = function (bufnr)
+            local gitsigns = require("gitsigns")
+
+            local function map(mode, l, r, opts)
+                opts = opts or {}
+                opts.buffer = bufnr
+                vim.keymap.set(mode, l, r, opts)
+            end
+
+            map("n", "<leader>hd", gitsigns.diffthis)
+        end
+    }
+}

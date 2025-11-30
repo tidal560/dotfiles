79d84be854951b3d6feb175ed3fce3fd7361674f
tidal560 <tidalwave560@anglernook.com>
2025-11-30 22:06:04 +0000

    move keymaps to global keymap file

--- .config/nvim/lua/plugins/nvim_tree.lua
+++ .config/nvim/lua/plugins/nvim_tree.lua
@@ -3,6 +3,12 @@ return {
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
+    init = function ()
+        -- recommended settings from nvim-tree documentation
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+    end,
+
     opts = {
         view = {
             width = 35,
@@ -24,17 +30,4 @@ return {
         },
     },
 
-    init = function()
-        -- recommended settings from nvim-tree documentation
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-
-        -- set keymaps
-        local keymap = vim.keymap -- for conciseness
-
-        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-    end,
 }
